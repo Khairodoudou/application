ba4d834a71a58ff46a7cd94cfc597f106a843506
Khairo23 <kheireddinedebz@gmail.com>
2026-04-04 00:26:40 +0200

    fix: use env DATABASE_URL in schema

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,16 @@
 import { PrismaClient } from '@prisma/client'
-import { createClient } from '@libsql/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
 const createPrismaClient = () => {
-  const libsql = createClient({
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
-  })
-  
   return new PrismaClient({
-    datasourceUrl: process.env.TURSO_DATABASE_URL,
+    datasources: {
+      db: {
+        url: process.env.TURSO_DATABASE_URL,
+      }
+    }
   })
 }
 
